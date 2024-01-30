6deac35dfb8f7efe46fe27ae65ca138c446b9cc3
chaconfrank <fchacon@webs314.com>
2024-01-30 07:41:39 +0100

    fifth day class

--- src/componets/Main.jsx
+++ src/componets/Main.jsx
@@ -1,13 +1,21 @@
 import React from "react";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 import RepositoryList from "./RepositryList";
 import AppBar from "./AppBar";
+import { Route, Switch } from "react-router-native";
 
 const Main = () => {
   return (
-    <View style={{ flexGrow: 1 }}>
+    <View style={{ flex: 1 }}>
       <AppBar />
-      <RepositoryList />
+      <Switch>
+        <Route path="/" exact>
+          <RepositoryList />
+        </Route>
+        <Route path="/signin" exact>
+          <Text>Working on it!</Text>
+        </Route>
+      </Switch>
     </View>
   );
 };
