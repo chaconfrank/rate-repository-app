913e492b0d3885427b1fe6a9e42c7395efa7d683
chaconfrank <fchacon@webs314.com>
2024-01-31 07:45:34 +0100

    sixth day

--- src/componets/Main.jsx
+++ src/componets/Main.jsx
@@ -3,6 +3,7 @@ import { Text, View } from "react-native";
 import RepositoryList from "./RepositryList";
 import AppBar from "./AppBar";
 import { Route, Switch } from "react-router-native";
+import Login from "../pages/Login";
 
 const Main = () => {
   return (
@@ -13,7 +14,7 @@ const Main = () => {
           <RepositoryList />
         </Route>
         <Route path="/signin" exact>
-          <Text>Working on it!</Text>
+          <Login />
         </Route>
       </Switch>
     </View>
